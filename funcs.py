ef86ce19048b713bef27b6ac36adc44eef5a0cfa
Mohamed-AliY <yazid.mohamedali01@gmail.com>
2022-01-03 20:29:14 +0100

    Ajout de la fonction mediane

--- funcs.py
+++ funcs.py
@@ -4,5 +4,18 @@ def min_int(a,b):
     else :
         return a
 
+def mediane(l):
+    l = sorted(l)
+    l_len = len(l)
+    if l_len < 1:
+        return None
+    if l_len % 2 == 0 :
+        print(( l[(l_len)//(2)-1] + l[(l_len)//2] ) / 2.0)
+        return ( l[(l_len)//(2)-1] + l[(l_len)//2] ) / 2.0
+    else:
+        print(l[(l_len-1)//2])
+        return l[(l_len-1)//2]
+
+
 
 # def 
