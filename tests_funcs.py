e9d3bc0f45869f2cd321f44641b11f4d50bbd5a9
Mohamed-AliY <yazid.mohamedali01@gmail.com>
2022-01-03 20:28:56 +0100

    Ajout du test la fonction mediane

--- tests_funcs.py
+++ tests_funcs.py
@@ -9,5 +9,12 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs.min_int(-1,2),-1)
         self.assertEqual(funcs.min_int(0,0),0)
 
+
+
+    def test_mediane(self):
+        self.assertEqual(funcs.mediane([0,1,2,3]),1.5)
+        self.assertEqual(funcs.mediane([8,15,20,4,11]),11)
+        # self.assertEqual(funcs.mediane())
+
 if __name__ == '__main__':
     unittest.main()
