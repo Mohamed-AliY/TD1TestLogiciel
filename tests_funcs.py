ea1d4e6ce7ce5d6a3c1f2ab97ffbcda92475d2c0
Mohamed-AliY <yazid.mohamedali01@gmail.com>
2022-01-03 21:21:04 +0100

    Ajout du test de la focntion ecart-type

--- tests_funcs.py
+++ tests_funcs.py
@@ -19,5 +19,11 @@ class TestFuncs(unittest.TestCase):
     def test_moyenne(self):
         self.assertEqual(funcs.moyenne([0,1,2,3]),1.5)
         self.assertEqual(funcs.moyenne([8,15,20,4,11]),11.6)
+        self.assertEqual(funcs.moyenne([5,5,5,5,5]),5)
+
+    def test_moyenne(self):
+        self.assertEqual(funcs.ecart_type([1,3,9,4]),2.947456530637899)
+        self.assertEqual(funcs.ecart_type([1,1,1,1]),0)
+
 if __name__ == '__main__':
     unittest.main()
