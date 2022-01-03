1a5bfde68b038414745aa2131d63f405559bf20e
Mohamed-AliY <yazid.mohamedali01@gmail.com>
2022-01-03 20:52:27 +0100

    Ajout de la fonction moyenne

--- funcs.py
+++ funcs.py
@@ -10,12 +10,18 @@ def mediane(l):
     if l_len < 1:
         return None
     if l_len % 2 == 0 :
-        print(( l[(l_len)//(2)-1] + l[(l_len)//2] ) / 2.0)
+        # print(( l[(l_len)//(2)-1] + l[(l_len)//2] ) / 2.0)
         return ( l[(l_len)//(2)-1] + l[(l_len)//2] ) / 2.0
     else:
-        print(l[(l_len-1)//2])
+        # print(l[(l_len-1)//2])
         return l[(l_len-1)//2]
 
-
-
+def moyenne(l):
+    somme = sum(l)
+    nb_elements = len(l)
+    moyenne = somme / nb_elements
+    # print("moyenne ",moyenne)
+    # print("nb_elements ",nb_elements)
+    # print("somme",somme)
+    return moyenne
 # def 
