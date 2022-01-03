72bddb5f46c653390f66666ea3656ec27cd19229
Mohamed-AliY <yazid.mohamedali01@gmail.com>
2022-01-03 13:41:38 +0100

    Ajout de la fonction de test de minimum

---
+++ tests_funcs.py
@@ -0,0 +1,13 @@
+import funcs
+import unittest
+
+class TestFuncs(unittest.TestCase):
+
+    def test_min_int(self):
+        self.assertEqual(funcs.min_int(0,2),0)
+        self.assertEqual(funcs.min_int(-1,-5),-5)
+        self.assertEqual(funcs.min_int(-1,2),-1)
+        self.assertEqual(funcs.min_int(0,0),0)
+
+if __name__ == '__main__':
+    unittest.main()
