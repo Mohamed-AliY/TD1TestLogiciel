475747217cec2f0124f64e09333b7faa5237f2c8
Mohamed-AliY <yazid.mohamedali01@gmail.com>
2022-01-05 20:41:25 +0100

    Ajout du test de la question 1 de l'exercice 2(pas complet)

--- tests_ex2.py
+++ tests_ex2.py
@@ -1,10 +1,20 @@
 import unittest
 import ex2
+import sqlite3
 
 class TestEx2(unittest.TestCase):
 
 	def test_ajouteUtilisateur(self):
-		
+		ex2.ajouteUtilisateur("toti","dzxhf215","vdv","gsg","fsv","fsd")
+		connexion=sqlite3.connect("basededonnee.db")
+		cursor=connexion.execute("SELECT * FROM UTILISATEUR")
+		for row in cursor:
+			print("pseudo = ", row[0])
+			print("mdp = ", row[1])
+			print("spublickey = ", row[2])
+			print("sprivatekey = ", row[3])
+			print("epublickey = ", row[4])
+			print("eprivatekey = ", row[5], "\n")
 
 if __name__ == '__main__':
     unittest.main()
