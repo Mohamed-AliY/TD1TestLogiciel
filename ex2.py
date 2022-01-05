497c7463908e48ee6a901a7e1c2cf5e628b59fde
Mohamed-AliY <yazid.mohamedali01@gmail.com>
2022-01-05 20:42:25 +0100

    Ajout du code de la question 1 de l'exercice 2

--- ex2.py
+++ ex2.py
@@ -5,10 +5,10 @@ import sqlite3
 def ajouteUtilisateur(pseudo,mdp,spublickey,sprivatekey,epublickey,eprivatekey):
 	
 	connexion=sqlite3.connect("basededonnee.db")
-	curseur=connexion.cursor()
-	curseur.execute('''CREATE TABLE IF NOT EXISTS UTILISATEUR(pseudo TEXT,mdp TEXT,
+	# curseur=connexion.cursor()
+	connexion.execute('''CREATE TABLE IF NOT EXISTS UTILISATEUR(pseudo TEXT,mdp TEXT,
 				spublickey TEXT,sprivatekey TEXT,epublickey TEXT,eprivatekey TEXT)''' )
 	donnees=(pseudo,mdp,spublickey,sprivatekey,epublickey,eprivatekey)
-	curseur.execute("INSERT INTO UTILISATEUR (pseudo,mdp,spublickey,sprivatekey,epublickey,eprivatekey) VALUES (?,?,?,?,?,?)",donnees)
+	connexion.execute("INSERT INTO UTILISATEUR (pseudo,mdp,spublickey,sprivatekey,epublickey,eprivatekey) VALUES (?,?,?,?,?,?)",donnees)
 	connexion.commit()
 	connexion.close()
